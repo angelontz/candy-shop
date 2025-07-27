041f58f261968e51e24332d3a97535a66bccfab5
angelontz <angel.lp.soad69@gmail.com>
2025-07-27 09:00:43 +0300

    works finaly for google

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { AuthProvider } from './context/AuthContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
@@ -20,12 +20,9 @@ root.render(
         Error: Google Client ID is not set. Please set REACT_APP_GOOGLE_CLIENT_ID in your environment variables.
       </div>
     )}
-reportWebVitals(console.log);
-);
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
