ad6b37f077703220df6ec37cc4a2bd3131fd4f18
angelontz <angel.lp.soad69@gmail.com>
2025-07-28 09:09:06 +0300

    navbar fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
 
 function Header({ cartItems = [], user, logout, mobileOpen, setMobileOpen }) {
   const location = useLocation();
